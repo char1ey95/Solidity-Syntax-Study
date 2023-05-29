31ad1c720b0ffb4f192007b02e537cff3e7cb558
char1ey <ckstn410@gmail.com>
2023-05-29 18:25:07 +0900

    fixed: 8강 학습 완료

--- migrations/01_deploy_Lec1.js
+++ migrations/01_deploy_Lec1.js
@@ -1,10 +1,22 @@
 // const Hello = artifacts.require("Hello");
 // const World = artifacts.require("World");
+
 // const DataType = artifacts.require("Data_type");
+
 // const Unit = artifacts.require("Unit");
+
 // const Exam_Func = artifacts.require("Exam_Func");
-const State = artifacts.require("State");
+
+// const State = artifacts.require("State");
+
+// const HowIsItSaved = artifacts.require("HowIsItSaved");
+
+const InstanceA = artifacts.require("InstanceA")
+const InstanceB = artifacts.require("InstanceB")
+const InstanceC = artifacts.require("InstanceC")
 
 module.exports = (deployer) => {
-  deployer.deploy(State)
+  deployer.deploy(InstanceA)
+  deployer.deploy(InstanceB)
+  deployer.deploy(InstanceC)
 };
